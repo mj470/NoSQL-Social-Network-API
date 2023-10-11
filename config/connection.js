29e40da77be4c302bd3e46efc92a642dfc4d0e87
Maren Christensen <marenjoychristensen@gmail.com>
2023-10-11 15:37:33 -0500

    created the mongoose connection to mongoDB

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+const mongoose
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+   useNewUrlParse: true,
+    useUnifiedTopology: true,
+});
+
+module.exports = mongoose.connection;
