6b6950791c9dd27c65310c5844552dbceb07af1e
Maren Christensen <marenjoychristensen@gmail.com>
2023-10-12 15:00:10 -0500

    changed file paths to routes and deleted redundant index.js file

--- routes/api-routes/index.js
+++ routes/api-routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
+const userRoutes = require('./user-routes');
 
-router.use('/user', userRoutes);
-router.use('/thought', thoughtRoutes);
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
