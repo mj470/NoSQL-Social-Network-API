acedefe38a5aceb9646ce3e3071f7b2693187610
Maren Christensen <marenjoychristensen@gmail.com>
2023-10-13 13:59:57 -0500

    added root level index.js file to routes to trouble shoot.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./config/connection');
-const routes = require('./routes/api-routes');
+const routes = require('./routes');
 const PORT = 3001;
 const app = express();
 
