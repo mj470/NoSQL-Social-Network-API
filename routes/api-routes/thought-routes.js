969e0b40a97844fa09dd9b9531afab95ad7719e7
Maren Christensen <marenjoychristensen@gmail.com>
2023-10-12 21:15:09 -0500

    changed order in routes for troubleshooting delete route

--- routes/api-routes/thought-routes.js
+++ routes/api-routes/thought-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const {
     getAllThoughts,
-    getThoughtById,
     createThought,
+    getThoughtById,
     updateThought,
     deleteThought,
     addReaction,
