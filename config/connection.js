b8b5db4a76f9780bdf19530df226732744383a00
Maren Christensen <marenjoychristensen@gmail.com>
2023-10-12 20:08:44 -0500

    changed connection to mongose and fixed port issue in server.

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,5 @@
-const mongoose = require('mongoose');
+const { connect, connection } = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-   useNewUrlParse: true,
-    useUnifiedTopology: true,
-});
+connect('mongodb://127.0.0.1:27017/social-network-api');
 
-module.exports = mongoose.connection;
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes/api-routes');
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
